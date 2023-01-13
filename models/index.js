086fb26611c26abc3bcda2d023caf4ae748dcd64
Sam Craig <samfcraig@gmail.com>
2023-01-13 14:41:39 -0500

    it's all done

---
+++ models/index.js
@@ -0,0 +1,25 @@
+const Location = require('./Location');
+const Traveller = require('./Traveller');
+const Trip = require('./Trip');
+
+// ASSOCIATIONS
+Location.belongsToMany(Traveller, {
+    through: {
+        model: Trip,
+        unique: false,
+    },
+});
+Traveller.belongsToMany(Location, {
+    through: {
+        model: Trip,
+        unique: false,
+    },
+});
+
+Traveller.hasMany(Trip);
+Location.hasMany(Trip);
+
+Trip.belongsTo(Location);
+Trip.belongsTo(Traveller);
+
+module.exports = { Location, Traveller, Trip };
---
+++ routes/api/travellerRoutes.js
@@ -0,0 +1,57 @@
+const router = require('express').Router();
+const { Location, Traveller, Trip } = require('../../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const travellerData = await Traveller.findAll();
+        res.json(travellerData);
+    } catch (err) {
+        console.error(err);
+        res.json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const travellerData = await Traveller.findByPk(req.params.id, {
+            include: [{
+                model: Trip,
+                attributes: [
+                    'id',
+                    'traveller_amount',
+                    'trip_budget',
+                ],
+                include: [{
+                    model: Location,
+                }]
+            }]
+        });
+        res.json(travellerData);
+    } catch (err) {
+        console.error(err);
+        res.json(err);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const travellerData = await Traveller.create(req.body);
+        res.json(travellerData);
+    } catch (err) {
+        console.error(err);
+        res.json(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const travellerData = await Traveller.destroy({ where: { id: req.params.id } });
+        res.json(travellerData);
+    } catch (err) {
+        console.error(err);
+        res.json(err);
+    }
+});
+
+
+module.exports = router;
